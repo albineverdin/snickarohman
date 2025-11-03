282f1a0b93dd6b08036f4f235c239f6bee1e3cfb
Albin <ens22aln@cs.umu.se>
2025-11-03 03:38:25 +0100

    Added banner button functionality

--- snickarohman/src/components/Banner/Banner.jsx
+++ snickarohman/src/components/Banner/Banner.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import './Banner.css';
 
-function Banner({ imageSrc, imageAlt, topper, title }) {
+function Banner({ imageSrc, imageAlt, topper, title, buttonText, buttonLink }) {
+    const handleButtonClick = () => {
+        if (buttonLink) {
+            const element = document.querySelector(buttonLink);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    };
+
     return (
         <div className="banner">
             <div className="banner__image-wrapper">
@@ -12,6 +21,11 @@ function Banner({ imageSrc, imageAlt, topper, title }) {
             <div className="banner__content">
                 {topper && <span className="banner__title-topper">{topper}</span>}
                 {title && <h1 className="banner__title">{title}</h1>}
+                {buttonText && buttonLink && (
+                    <button className="banner__button" onClick={handleButtonClick}>
+                        {buttonText}
+                    </button>
+                )}
             </div>
         </div>
     );
